6d87c7e26d0bb4b6cf2f236da65fb7efd1cc8bda
DEV1 <DEV1@gmail.com>
2020-10-22 19:14:04 +0530

    stash m3 commit by dev1

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -6,15 +6,21 @@ public class Utility {
 	public Date getsysDate() {
 		return new Date();
 	}
+
 	public void m1() {
 
 		System.out.println("Utility m1() method:1234");
 		System.out.println("abc123");
 	}
+
 	public void m2() {
 		System.out.println("m2()");
 	}
+
 	public void m4() {
 		System.out.println("m4()");
+	}
+	public void m3() {
+System.out.println("m3()");
 	}
 }
