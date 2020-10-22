84ba3628a874c171da3ed6f7dcdcfa52d3b62db2
DEV1 <DEV1@gmail.com>
2020-10-22 17:29:36 +0530

    m1() created created by Dev1

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -3,7 +3,10 @@ package com.nt.beans;
 import java.util.Date;
 
 public class Utility {
-	public Date date() {
+	public Date getsysDate() {
 		return new Date();
+	}
+	public void m1() {
+		System.out.println("Utility m1() method");
 	}
 }
